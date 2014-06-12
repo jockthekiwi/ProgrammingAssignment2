12c46d6e55d13f42d13e24918d5155fb405f7190
jockthekiwi <gavinjolly@Gavins-MacBook-Air.local>
2014-06-13 07:29:12 +1200

    first

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Assignment 2
+## 12 June 2014
+## Author: jockthekiwi
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        ## m.c is the global variable for the Matix cache
+        m.c <- NULL
+        set <- function(y) {
+                x <<- y
+                m.c <<- NULL
+        }
+        get <- function() x
+        setinvert <- function(solve) m.c <<- solve
+        getinvert <- function() m.c
+        list(set = set, get = get,
+             setinvert = setinvert,
+             getinvert = getinvert)
+        
 }
 
 
@@ -12,4 +26,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m<-x$getinvert()
+        if(!is.null(m.c)) {
+                message("getting cached data")
+                return(m.c)
+        }
+        data <- x$get()
+        m.c <- solve(data)
+        x$setinvert(m.c)
+        m.c
+        
 }
